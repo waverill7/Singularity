55a7ecee652d89f65f8cb0bebff133f8ebcee7c0
waverill7 <waverill@lion.lmu.edu>
2014-03-05 18:04:21 -0800

    Update test-scanner.js

--- test/test-scanner.js
+++ test/test-scanner.js
@@ -76,7 +76,7 @@ describe('The scanner', function () {
       i(tokens[53]).should.equal(i({kind:'Dedent',lexeme:'Dedent',line:11,col:1}))
       i(tokens[54]).should.equal(i({kind:'Dedent',lexeme:'Dedent',line:11,col:1}))
       i(tokens[55]).should.equal(i({kind:'Return',lexeme:'Return',line:11,col:1}))
-      i(tokens[57]).should.equal(i({kind:'EOF',lexeme:'EOF'}))
+      i(tokens[56]).should.equal(i({kind:'EOF',lexeme:'EOF'}))
       done()
     })
   })
