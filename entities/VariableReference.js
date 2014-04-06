e56de6def55fd2dfe15a71202b5cede6d9038604
waverill7 <waverill@lion.lmu.edu>
2014-04-05 23:53:56 -0700

    Delete VariableReference.js

--- entities/VariableReference.js
+++
@@ -1,9 +0,0 @@
-function VariableReference(token) {
-    this.token = token;
-}
-
-VariableReference.prototype.toString = function () {
-    return '(VariableReference ' + this.token.lexeme + ')';
-}
-
-module.exports = VariableReference;
