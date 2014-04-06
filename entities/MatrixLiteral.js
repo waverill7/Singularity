c7520223d20baadb971653552bd60558c2578286
waverill7 <waverill@lion.lmu.edu>
2014-04-05 23:52:46 -0700

    Delete MatrixLiteral.js

--- entities/MatrixLiteral.js
+++
@@ -1,9 +0,0 @@
-function MatrixLiteral(expressions) {
-    this.expressions = expressions;
-}
-
-MatrixLiteral.prototype.toString = function () {
-    return '(MatrixLiteral ' + this.expressions.join(' ') + ')';
-}
-
-module.exports = MatrixLiteral;
