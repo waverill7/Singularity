746f682ed5d13b9628678d051e046c7a22bbcd9b
waverill7 <waverill@lion.lmu.edu>
2014-04-05 23:52:43 -0700

    Delete IntegerLiteral.js

--- entities/IntegerLiteral.js
+++
@@ -1,9 +0,0 @@
-function IntegerLiteral(token) {
-    this.token = token;
-}
-
-IntegerLiteral.prototype.toString = function () {
-    return '(IntegerLiteral ' + this.token.lexeme + ')';
-}
-
-module.exports = IntegerLiteral;
