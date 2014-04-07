02639bd3d1c5a9547802f20ab6350f45d1cdb1b9
waverill7 <waverill@lion.lmu.edu>
2014-04-06 20:43:22 -0700

    Update ConditionalStatement.js

--- entities/ConditionalStatement.js
+++ entities/ConditionalStatement.js
@@ -5,12 +5,12 @@ function ConditionalStatement(IF, ELIF, ELSE) {
 }
 
 ConditionalStatement.prototype.toString = function () {
-    ifPartition = '(if ' + this.IF + ')';
-    elifPartitions = [];
+    var ifPartition = '(if ' + this.IF + ')';
+    var elifPartitions = [];
     for (var i = 0; i < this.ELIF.length; i += 2) {
     	elifPartitions.push('(elif ' + this.ELIF[i] + ' ' + this.ELIF[i+1] + ')');
     }
-    elsePartition = '(else ' + this.ELSE + ')';
+    var elsePartition = '(else ' + this.ELSE + ')';
     return '(ConditionalStatement ' + ifPartition + ' ' + elifPartitions.join(' ') + ' ' + elsePartition + ')';
 } 
 
