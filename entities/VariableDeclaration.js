a4cb5901c117b539dbe5581a6fc552b913a3a7fd
waverill7 <waverill@lion.lmu.edu>
2014-04-29 17:54:33 -0700

    Update Analyzer

--- entities/VariableDeclaration.js
+++ entities/VariableDeclaration.js
@@ -8,4 +8,15 @@ VariableDeclaration.prototype.toString = function () {
     return '(VariableDeclaration ' + this.scope + ' ' + this.name.lexeme + ' ' + this.expression + ')';
 } 
 
+VariableDeclaration.prototype.analyze = function (context) {
+	if (this.scope === 'global') {
+        context.globalVariableMustNotBeAlreadyDeclared(this.name);
+        context.addGlobalVariable(this.name.lexeme, this);
+	} else {
+        context.localVariableMustNotBeAlreadyDeclared(this.name);
+        context.addLocalVariable(this.name.lexeme, this);
+	}
+	this.expression.analyze(context);
+}
+
 module.exports = VariableDeclaration;
