ea0b85b328681fe5e3ec414c4524700425126cd6
waverill7 <waverill@lion.lmu.edu>
2014-04-30 00:31:30 -0700

    Update Analyzer

--- entities/ObjectDeclaration.js
+++ entities/ObjectDeclaration.js
@@ -9,4 +9,20 @@ ObjectDeclaration.prototype.toString = function () {
     return '(ObjectDeclaration ' + this.scope + ' ' + this.name.lexeme + ' ' + this.inheritance.join(' ') + ' ' + this.body + ')';
 } 
 
+ObjectDeclaration.prototype.analyze = function (context) {
+	if (this.scope === 'global') {
+        context.globalVariableMustNotBeAlreadyDeclared(this.name);
+        context.addGlobalVariable(this.name.lexeme, this);
+    } else {
+        context.localVariableMustNotBeAlreadyDeclared(this.name);
+        context.addLocalVariable(this.name.lexeme, this);
+    }
+    var objectContext = context.createChildContext();
+        this.inheritance.forEach(function (object) {
+            object.analyze(objectContext);
+        });
+    }
+    this.body.analyze(objectContext, 'ObjectDeclaration');
+}
+
 module.exports = ObjectDeclaration;
