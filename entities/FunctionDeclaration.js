c11c937bfd2c1357746d2898938e793cbe0527c0
waverill7 <waverill@lion.lmu.edu>
2014-04-29 23:38:27 -0700

    Update Analyzer

--- entities/FunctionDeclaration.js
+++ entities/FunctionDeclaration.js
@@ -8,6 +8,26 @@ function FunctionDeclaration(scope, name, parameters, value, body) {
 
 FunctionDeclaration.prototype.toString = function () {
     return '(FunctionDeclaration ' + this.scope + ' ' + this.name.lexeme + ' ' + this.parameters.join(' ') + ' ' + this.value + ' ' + this.body + ')';
-} 
+}
+
+FunctionDeclaration.prototype.analyze = function (context) {
+    if (this.scope === 'global') {
+        context.globalVariableMustNotBeAlreadyDeclared(this.name);
+        context.addGlobalVariable(this.name.lexeme, this);
+    } else {
+        context.localVariableMustNotBeAlreadyDeclared(this.name);
+        context.addLocalVariable(this.name.lexeme, this);
+    }
+    var functionContext = context.createChildContext();
+    if (this.parameters[0] !== 'void') {
+        this.parameters.forEach(function (parameter) {
+            parameter.analyze(functionContext);
+        });
+    }
+    if (this.value !== 'void') {
+        this.value.analyze(functionContext);
+    }
+    this.body.analyze(functionContext, 'FunctionDeclaration');
+}
 
 module.exports = FunctionDeclaration;
