706480da13bdf8e62ea7339ae5d56e7febdb54b9
waverill7 <waverill@lion.lmu.edu>
2014-04-30 18:26:15 -0700

    Update Analyzer

--- entities/MethodDeclaration.js
+++ entities/MethodDeclaration.js
@@ -1,3 +1,5 @@
+var error = require('../error');
+
 function MethodDeclaration(scope, name, parameters, value, body) {
     this.scope = scope;
     this.name = name;
@@ -9,5 +11,28 @@ function MethodDeclaration(scope, name, parameters, value, body) {
 MethodDeclaration.prototype.toString = function () {
     return '(MethodDeclaration ' + this.scope + ' ' + this.name + ' ' + this.parameters.join(' ') + ' ' + this.value + ' ' + this.body + ')';
 } 
+
+MethodDeclaration.prototype.analyze = function (context) {
+    if (!context.lookupContextType('ObjectDeclaration')) {
+        error('A "method" declaration must be within the context of an "object" declaration.');
+    }
+    if (this.scope === 'global') {
+        context.globalVariableMustNotBeAlreadyDeclared(this.name);
+        context.addGlobalVariable(this.name.lexeme, this);
+    } else {
+        context.localVariableMustNotBeAlreadyDeclared(this.name);
+        context.addLocalVariable(this.name.lexeme, this);
+    }
+    var methodContext = context.createChildContext();
+    this.parameters.forEach(function (parameter) {
+    	if (parameter !== 'self') {
+            parameter.analyze(methodContext);
+        }
+    });
+    if ((this.value !== 'void') && (this.value !== 'self')) {
+    	this.value.analyze(methodContext);
+    }
+    this.body.analyze(methodContext, 'MethodDeclaration');
+}
 
 module.exports = MethodDeclaration;
