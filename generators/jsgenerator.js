22a69e81927e965fa275764e328e15d1a9bcf63e
waverill7 <waverill@lion.lmu.edu>
2014-05-08 08:29:36 -0700

    Update Generator

---
+++ generators/jsgenerator.js
@@ -0,0 +1,103 @@
+var util = require('util');
+var HashMap = require('hashmap').HashMap;
+
+module.exports = function (program) {
+    gen(program);
+}
+
+var indentPadding = 4;
+var indentLevel = 0;
+
+function emit(line) {
+    var pad = indentPadding * indentLevel;
+    console.log(Array(pad+1).join('') + line);
+}
+
+function makeOp(op) {
+    return {not: '!', and: '&&', or: '||'}[op] || op;
+}
+
+var makeVariable = (function () {
+    var lastId = 0;
+    var map = new HashMap();
+    return function (v) {
+        if (!map.has(v)) {
+            map.set(v, ++lastId);
+        }
+        return '_v' + map.get(v);
+    }
+}());
+
+function gen(e) {
+    return generator[e.constructor.name](e);
+}
+
+var generator = {
+
+    'Block': function (block) {
+        indentLevel++;
+        block.statements.forEach(function (statement) {
+            gen(statement);
+        });
+        indentLevel--;
+    },
+
+    'BooleanLiteral': function (literal) {
+        return literal.toString();
+    },
+
+    'BreakStatement': function(s) {
+        return 'break';
+    },
+
+    'CharacterLiteral': function (literal) {
+        return literal.toString();
+    },
+
+    'ContinueStatement': function (s) {
+        return 'continue';
+    },
+
+    'ForStatement': function (s) {
+        emit('for (' + gen(s.initialization) + '; ' + gen(s.condition) + '; ' + gen(s.update) + ') {');
+        gen(s.body);
+        emit('}');
+    },
+
+    'InfixExpression': function (e) {
+        return util.format('(%s %s %s)', gen(e.left), makeOp(e.operator.lexeme), gen(e.right));
+    },
+
+    'IntegerLiteral': function (literal) {
+        return literal.toString();
+    },
+
+    'PrefixExpression': function (e) {
+        return util.format('(%s %s)', makeOp(e.operator.lexeme), gen(e.operand));
+    },
+
+    'Program': function (program) {
+        indentLevel = 0;
+        emit('(function () {');
+        gen(program.block);
+        emit('}());');
+    },
+
+    'RealLiteral': function (literal) {
+        return literal.toString();
+    },
+
+    'StringLiteral': function (literal) {
+        return literal.toString();
+    },
+
+    'VoidLiteral': function (literal) {
+        return 'null';
+    },
+
+    'WhileStatement': function (s) {
+        emit('while (' + gen(s.condition) + ') {');
+        gen(s.body);
+        emit('}');
+    }
+}
